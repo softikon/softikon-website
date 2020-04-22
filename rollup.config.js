917e91698c912d7eebce3af0fb0c1ffe3da419ef
Máčal Petr <petr.macal@avast.com>
2020-04-22 23:03:38 +0200

    Tailwind css system

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,33 @@ import svelte from 'rollup-plugin-svelte'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
+// csstailwind related plugins (postcss)
+import sveltePreprocess from 'svelte-preprocess'
+
 import config from 'sapper/config/rollup'
 import pkg from './package.json'
+
+const tailwindConfig = require('./tailwind.config')
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 const legacy = !!process.env.SAPPER_LEGACY_BUILD
+
+const postcssPlugins = {
+  dev: [
+    require('tailwindcss')(tailwindConfig)
+  ],
+  prod: [
+    require('tailwindcss')(tailwindConfig)
+    // require('tailwindcss')(tailwind),
+    // require('postcss-import')(),
+    // require('postcss-url')(),
+    // require('autoprefixer'),
+    // require('postcss-preset-env'),
+    // purgecss,
+    // cssnano,
+  ]
+}
 
 export default {
   client: {
@@ -20,6 +41,11 @@ export default {
         'process.env.NODE_ENV': JSON.stringify(mode)
       }),
       svelte({
+        preprocess: [sveltePreprocess({
+          postcss: {
+            plugins: postcssPlugins.dev
+          }
+        })],
         dev,
         hydratable: true,
         emitCss: true
@@ -40,6 +66,11 @@ export default {
         'process.env.NODE_ENV': JSON.stringify(mode)
       }),
       svelte({
+        preprocess: [sveltePreprocess({
+          postcss: {
+            plugins: postcssPlugins.dev
+          }
+        })],
         dev,
         generate: 'ssr'
       }),
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      body: ['Montserrat']
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
