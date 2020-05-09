1ab0c176e3b5d58f8ccd9233558ff6dbe475024a
Petr Macal <czhakis@gmail.com>
2020-05-09 21:10:34 +0200

    viewportHeight readable state

--- src/store/ui.js
+++ src/store/ui.js
@@ -1,3 +1,19 @@
-import { writable } from 'svelte/store'
+import { writable, readable } from 'svelte/store'
 
 export const isHeaderInverted = writable(false)
+export const viewportHeight = readable(
+  null, set => {
+  const handler = () => {
+    set(window.innerHeight)
+  }
+  if (typeof window !== 'undefined') {
+    window.addEventListener('resize', handler)
+    handler()
+  }
+
+  return () => {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', handler)
+    }
+  }
+})
