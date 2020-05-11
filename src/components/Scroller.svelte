412aa1c0d287664b6d4fb476e67898fd0732a0fe
Máčal Petr <petr.macal@avast.com>
2020-05-11 15:34:13 +0200

    Smarter scroller

--- src/components/Scroller.svelte
+++ src/components/Scroller.svelte
@@ -1,14 +1,25 @@
 <script>
-  import { onMount } from 'svelte'
+  import { onMount, createEventDispatcher } from 'svelte'
   import { viewportHeight } from '../store/ui'
 
   let distance
   let el
   let fixed
+  let currentItem = 1
+
+  const dispatch = createEventDispatcher()
 
   export let autoWidth = false
+  export let length = 1
 
   $: fixed = distance === 0 || ((distance < 0) && Math.abs(distance - $viewportHeight) <= el.offsetHeight)
+  $: {
+    const innerDistanceRatio = fixed && (Math.abs(distance) / (el.offsetHeight - $viewportHeight))
+    if (innerDistanceRatio) {
+      currentItem = Math.floor(innerDistanceRatio * 100 / (100 / length))
+    }
+  }
+  $: dispatch('itemChanged', currentItem)
   $: finished = distance < 0 && !fixed
 
   onMount(() => {
