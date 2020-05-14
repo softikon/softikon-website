6d6c0820c16ad5884bce131402987c90833a6829
Máčal Petr <petr.macal@avast.com>
2020-05-14 18:46:03 +0200

    Work page proposal

--- src/routes/work/index.svelte
+++ src/routes/work/index.svelte
@@ -3,83 +3,93 @@
   import Revealable from '../../components/Revealable.svelte'
   import LineBlock from '../../components/LineBlock.svelte'
   import TransitionWrapper from '../../components/TransitionWrapper.svelte'
+  import Scroller from '../../components/Scroller.svelte'
+  import Button from '../../components/Button.svelte'
   import { animate } from '../../helpers/intersectionObserver'
+
+  let currentItem = 0
+
+  const setItemManually = (index) => {
+    currentItem = index
+  }
  </script>
 
 <TransitionWrapper>
   <article>
-    <section class="pb-16 flex items-end center relative min-h-screen">
-      <div class="w-10/12 pt-64 md:pt-0 mx-auto flex flex-wrap flex-col">
-        <div class="flex flex-col w-full md:w-9/12">
-            <h1 class="h1--sub alternative-font">
-              <span>
-                <Revealable><span class="block">Under</span> the hood</Revealable>
-              </span>
-            </h1>
-        </div>
-        <div class="w-full md:w-9/12">
-          <p data-aos="fade-up" class="tracking-tight pt-16 leading-loose font-medium text-2xl text-gray-600">
-            Nechat za sebe mluvit odvedenou práci není klišé. Naopak. Úspěšné produkty se staly úspěšným především proto, že se v nich spojuje nápad, vášeň, talent, zkušenosti, lidé, velká přidaná hodnota, precizní technické zpracování a od počátku perfektní, promyšlená strategie a komunikace. Jedině tak se z projektu úspěšný produkt.
-          </p>
-        </div>
+    <section class="flex min-h-screen relative">
+      <div class="section-bg light"></div>
+      <div class="absolute inset-y-0 right-0 w-24 z-20">
+        <Scroller autoWidth>
+          <ul class="h-screen flex flex-col justify-center">
+            {#each [1,2] as item, index}
+              <li class="cursor-pointer pagination px-4 lg:px-12 py-8 font-bold" on:click={() => setItemManually(index)} class:active={index === currentItem}>{item}</li>
+            {/each}
+          </ul>
+        </Scroller>
       </div>
-    </section>
-
-    <section class="relative pt-64" id="showcase">
-      <div class="section-bg light" use:animate data-animate="slideScaleLeft"></div>
-      <div class="w-10/12 mx-auto flex flex-col">
-        <LineBlock data-aos="fade-up" class="mb-8">
-          <h3 class="mb-8 text-3xl lg:text-3xl">Komerční produkty</h3>
-        </LineBlock>
-        <LineBlock noLine>
-          <div class="flex flex-col md:flex-row">
-            <div class="w-full md:w-3/6">
-              <div data-aos="fade-up" class="pb-8 md:pr-4">
-                <Card data-concise="true" to="work/autopublisher" background="#0f041b" image="img/autopublisher-mock.png">
-                  <span class="text-white alternative-font text-4xl" slot="sub">Autopublisher</span>
-                  <span slot="title">Specializovaný software pro autobazary, který šetří čas a peníze.</span>
-                  <a class="text-3xl text-white font-bold underline" href="work/autopublisher">Více informací</a>
-                  <ul slot="labels">
-                    <li>Nápad, implementace</li>
-                    <li>Multiplatformní aplikace</li>
-                  </ul>
-                </Card>
-              </div>
-
-              <div data-aos="fade-up" class="pb-8 md:pr-4">
-                <Card data-concise="true" to="work/autopublisher" background="#0f041b" image="img/autopublisher-mock.png">
-                  <span class="text-white alternative-font text-4xl" slot="sub">Autopublisher</span>
-                  <span slot="title">Pracujeme na posledním DRM, který prodejci vozidel v České Republice a na Slovensku budu potřebovat</span>
-                  <a class="text-3xl text-white font-bold underline" href="work/autopublisher">Více informací</a>
-                  <ul slot="labels">
-                    <li>Nápad, implementace</li>
-                    <li>Multiplatformní aplikace</li>
-                  </ul>
-                </Card>
-              </div>
-            </div>
-
-            <div class="w-full md:w-3/6 md:pt-24">
-              <div data-aos="fade-up" class="pb-8 md:pl-4">
-                <Card data-concise="true" to="work/betsense" background="#d2dae5" image="img/betsense-mock.png">
-                  <span slot="sub" class="text-gray-600 alternative-font text-4xl">Betsense</span>
-                  <span slot="title" class="text-gray-900">Prémiový realtime hub pro sázkové kanceláře i sázkaře, který se pyšní nejnižší datovou latencí ve svém oboru.</span>
-                  <div class="text-xl text-gray-500">Spouštíme Q3/2020</div>
-                  <ul slot="labels" class="text-gray-900">
-                    <li>Data-intensive computing</li>
-                    <li>Inteligentní AI, 500+ mikroslužeb</li>
-                  </ul>
-                </Card>
-              </div>
-            </div>
+      <div class="absolute inset-0">
+        <Scroller length="2" on:itemChanged={({detail}) => setItemManually(detail)}>
+          <div class="relative w-8/12 lg:w-10/12 mx-auto h-screen flex flex-col justify-center">
+            <ul class="flex items-center flex-col relative w-5/12">
+              <li class="item" class:current={currentItem === 0}>
+                <LineBlock lg>
+                  <p class="mb-16 leading-loose md:text-2xl" style="color: rgba(17, 17, 17, 0.8);">
+                    Specializ;ovaný software pro autobazary, který šetří čas a peníze.
+                  </p>
+                  <Button href="test/pointer">detail</Button>
+                </LineBlock>
+              </li>
+              <li class="item" class:current={currentItem === 1}>
+                <LineBlock lg>
+                  <p class="mb-16 leading-loose md:text-2xl" style="color: rgba(17, 17, 17, 0.8);">
+                    Prémiový realtime hub pro sázkové kanceláře i sázkaře, který se pyšní nejnižší datovou latencí ve svém oboru.
+                  </p>
+                  <Button href="test/pointer">detail</Button>
+                </LineBlock>
+              </li>
+            </ul>
           </div>
-        </LineBlock>
+        </Scroller>
       </div>
-    </section>
+      <div style="margin-left: auto;">
+        <div class="flex flex-col" style="width: 50vw;">
+          <Card data-concise="true" to="work/autopublisher" background="#0f041b" image="img/autopublisher-mock.png">
+            <span class="text-white alternative-font text-4xl" slot="sub">Autopublisher</span>
+            <ul slot="labels">
+              <li>Nápad, implementace</li>
+              <li>Multiplatformní aplikace</li>
+            </ul>
+          </Card>
 
-    <section class="py-64 relative flex" id="opensource">
-      <div class="section-bg light" use:animate data-animate="slideScaleLeft"></div>
+          <Card data-concise="true" to="work/betsense" background="#d2dae5" image="img/betsense-mock.png">
+            <span slot="sub" class="text-gray-600 alternative-font text-4xl">Betsense</span>
+            <span slot="title" class="text-gray-900">Prémiový realtime hub pro sázkové kanceláře i sázkaře, který se pyšní nejnižší datovou latencí ve svém oboru.</span>
+            <div class="text-xl text-gray-500">Spouštíme Q3/2020</div>
+            <ul slot="labels" class="text-gray-900">
+              <li>Data-intensive computing</li>
+              <li>Inteligentní AI, 500+ mikroslužeb</li>
+            </ul>
+          </Card>
+        </div>
+      </div>
     </section>
   </article>
 </TransitionWrapper>
+
+<style>
+  .item {
+    position: absolute;
+    opacity: 0;
+    transition: opacity 0.69s;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100%;
+    left: 0;
+  }
+
+  .item.current {
+    opacity: 1;
+  }
+
+</style>
 
