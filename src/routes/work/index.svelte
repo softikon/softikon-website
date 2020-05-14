4a319a8abdc388f6f57c2f53eaf7988a1d5e9083
Máčal Petr <petr.macal@avast.com>
2020-05-15 00:23:18 +0200

    Works page WIP

--- src/routes/work/index.svelte
+++ src/routes/work/index.svelte
@@ -1,5 +1,6 @@
  <script>
   import { onMount } from 'svelte'
+  import { writable } from 'svelte/store'
   import Card from '../../components/Card.svelte'
   import Revealable from '../../components/Revealable.svelte'
   import LineBlock from '../../components/LineBlock.svelte'
@@ -9,55 +10,80 @@
   import { animate } from '../../helpers/intersectionObserver'
   import touchEvents, { install as installTouchEvents } from '../../helpers/touch'
 
-  let currentItem = 0
+  // This is little bit complex - refactor maybe?
+  // currentItem is binded to Scroller for getting the actual focused item when scroll and is the source of truth
+  // store contains state that is set manually and currentItem and scroller handler subscribes for the value
+  // currentItem value set by Scroller does not affect store and do not trigger scroller handler
+  let store = writable(0)
+  $: currentItem = $store
   const items = 2
 
+  let scrollerActive = false
+  let wantScroll = false
+
   const setItemManually = (index) => {
-    currentItem = index
+    wantScroll = true
+    store.set(index)
   }
 
-  const swipeLeft = () => setItemManually((currentItem + 1) % items)
-  const swipeRight = () => setItemManually((currentItem - 1) < 0 ? (items - 1) : (currentItem - 1))
+  const next = () => scrollerActive && setItemManually(($store + 1) % items)
+  const previous = () => scrollerActive && setItemManually(($store - 1) < 0 ? (items - 1) : ($store - 1))
 
   onMount(() => {
-    installTouchEvents()
+    const unsubscribe = store.subscribe(index => {
+      if (!wantScroll) return
+      const el = document.getElementById(`work-${index}`)
+      if (el) {
+        window.scroll({
+          top: el.getBoundingClientRect().top + window.scrollY,
+          behavior: 'smooth'
+        })
+      }
+    })
+    const uninstall = installTouchEvents()
+    return () => {
+      uninstall()
+      unsubscribe()
+    }
   })
 
-  touchEvents.on('swipeLeft', swipeLeft)
-  touchEvents.on('swipeRight', swipeRight)
+  touchEvents.on('swipeLeft', next)
+  touchEvents.on('swipeRight', previous)
  </script>
 
 <TransitionWrapper>
   <article>
     <section class="flex min-h-screen relative">
       <div class="section-bg light"></div>
-      <div class="absolute inset-y-0 right-0 w-24 z-20">
+      <div class="absolute inset-y-0 right-0 w-24" style="z-index: 1;">
         <Scroller autoWidth>
           <ul class="h-screen flex flex-col justify-center">
-            {#each [1,2] as item, index}
-              <li class="cursor-pointer pagination px-4 lg:px-12 py-8 font-bold" on:click={() => setItemManually(index)} class:active={index === currentItem}>{item}</li>
+            {#each ['AP', 'BS'] as item, index}
+              <li class="cursor-pointer pagination px-4 lg:px-12 py-8 bg-white bg-opacity-400 font-bold" on:click={() => setItemManually(index)} class:active={index === currentItem}>{item}</li>
             {/each}
           </ul>
         </Scroller>
       </div>
       <div class="absolute inset-0">
-        <Scroller length="2" on:itemChanged={({detail}) => setItemManually(detail)}>
-          <div class="relative w-8/12 lg:w-10/12 mx-auto h-screen flex flex-col justify-center">
+        <Scroller length="2" bind:currentItem={currentItem} on:stateChanged={e => scrollerActive = e.detail}>
+          <div class="relative w-10/12 mx-auto h-screen flex flex-col justify-center">
             <ul class="flex items-center flex-col relative w-5/12">
               <li class="item" class:current={currentItem === 0}>
-                <LineBlock lg>
-                  <p class="mb-16 leading-loose md:text-2xl" style="color: rgba(17, 17, 17, 0.8);">
-                    Specializ;ovaný software pro autobazary, který šetří čas a peníze.
+                <LineBlock data-aos="fade-right">
+                  <h4 class="font-bold text-lg uppercase leading-loose">Komerční produkt</h4>
+                  <p class="mb-16 leading-loose text-xl md:text-4xl" style="color: rgba(17, 17, 17, 0.8);">
+                    Specializovaný software pro autobazary, který šetří čas a peníze.
                   </p>
-                  <Button href="test/pointer">detail</Button>
+                  <Button href="work/autopublisher">detail</Button>
                 </LineBlock>
               </li>
               <li class="item" class:current={currentItem === 1}>
-                <LineBlock lg>
-                  <p class="mb-16 leading-loose md:text-2xl" style="color: rgba(17, 17, 17, 0.8);">
+                <LineBlock data-aos="fade-right">
+                  <h4 class="font-bold text-lg uppercase leading-loose">SAAS</h4>
+                  <p class="mb-16 leading-loose text-xl md:text-4xl" style="color: rgba(17, 17, 17, 0.8);">
                     Prémiový realtime hub pro sázkové kanceláře i sázkaře, který se pyšní nejnižší datovou latencí ve svém oboru.
                   </p>
-                  <Button href="test/pointer">detail</Button>
+                  <Button href="work/betsense">detail</Button>
                 </LineBlock>
               </li>
             </ul>
@@ -66,7 +92,7 @@
       </div>
       <div style="margin-left: auto;">
         <div class="flex flex-col" style="width: 50vw;">
-          <Card data-concise="true" to="work/autopublisher" background="#0f041b" image="img/autopublisher-mock.png">
+          <Card id="work-0" data-concise="true" to="work/autopublisher" background="#0f041b" image="img/autopublisher-mock.png">
             <span class="text-white alternative-font text-4xl" slot="sub">Autopublisher</span>
             <ul slot="labels">
               <li>Nápad, implementace</li>
@@ -74,7 +100,7 @@
             </ul>
           </Card>
 
-          <Card data-concise="true" to="work/betsense" background="#d2dae5" image="img/betsense-mock.png">
+          <Card id="work-1" data-concise="true" to="work/betsense" background="#d2dae5" image="img/betsense-mock.png">
             <span slot="sub" class="text-gray-600 alternative-font text-4xl">Betsense</span>
             <span slot="title" class="text-gray-900">Prémiový realtime hub pro sázkové kanceláře i sázkaře, který se pyšní nejnižší datovou latencí ve svém oboru.</span>
             <div class="text-xl text-gray-500">Spouštíme Q3/2020</div>
@@ -102,6 +128,7 @@
 
   .item.current {
     opacity: 1;
+    z-index: 2;
   }
 
 </style>
